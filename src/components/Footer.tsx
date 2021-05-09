dd2bb0689f0d4534ac6149bb2d73f79d5375f2d9
Yasamato <agent77326@gmail.com>
2021-05-09 20:30:50 +0200

    Fix docker tag nameing

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,9 +7,11 @@ const Footer = () => {
     return (
         <footer className={"mt-auto p-2"}>
             <Container>
-                Copyright (c) Leo Jung 2021, Images by <a href="https://icons8.com/">Icons8</a>
+                Copyright (c) <a href={"https://github.com/Yasamato"}>
+                Yasamato
+            </a> 2021, Images by <a href="https://icons8.com/">Icons8</a>
                 <span className={"float-right"}>
-                    <a href="https://github.com/yasamato/studi-watch" target="_blank" rel="noopener">
+                    <a href="https://github.com/Web-SyncPlay/Web-SyncPlay" target="_blank" rel="noopener">
                         <span className={"icon"}>
                             <FaGithub/>
                         </span>
